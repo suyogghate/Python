4376e9a27d597089b2121e7e9202f3a64f534415
suyogghate <suyogghate96@gmail.com>
2025-03-11 19:29:28 +0530

    email slicer program

--- strings.py
+++ strings.py
@@ -157,9 +157,19 @@
 
 # print(type(f"{2 * 30}"))
 
-def square(n):
-    '''Takes in a number n, returns the square of n'''
-    print(n**2)
+# def square(n):
+#     '''Takes in a number n, returns the square of n'''
+#     print(n**2)
 
-square(5)
-print(square.__doc__)          # prints the docstring inside a method
+# square(5)
+# print(square.__doc__)          # prints the docstring inside a method
+
+# Email slicer program
+email = input("Enter your email: ")
+
+# index = email.index("@")
+
+username = email[:email.index("@")]
+domain = email[email.index("@") + 1:]
+
+print(f"Your username is {username} and domain is {domain}.")
