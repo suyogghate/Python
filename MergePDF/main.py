c592925288fe1ed323e7a65e3405606694c01fac
suyogghate <suyogghate96@gmail.com>
2025-04-20 22:55:39 +0530

    pdf merger corrected

--- MergePDF/main.py
+++ MergePDF/main.py
@@ -4,9 +4,10 @@ import os
 merger = PdfMerger()
 # merger = PdfWriter()
 files = [file for file in os.listdir() if file.endswith(".pdf")]
+# files = ["dummy1.pdf", "dummy2.pdf"]
 
 for pdf in files:
     merger.append(pdf)
 
-merger.write("merged-pdf.pdf")
+merger.write("merged.pdf")
 merger.close()
